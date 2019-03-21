990d5f31f2ab176d30494a267e8caa06925248fd
GotohFumiaki <gf1010redgreen@gmail.com>
2019-03-21 12:13:43 +0900

    AddTriming拡張メソッドの追加

--- Assets/Scripts/MouseBehaviour.cs
+++ Assets/Scripts/MouseBehaviour.cs
@@ -1,8 +1,32 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
+
+public static class ListExtensions
+{
+    /// <summary>
+    /// List<T>にすでに追加されている要素を省いて追加してList<T>を返す
+    /// </summary>
+    /// <param name="addObj">追加したい要素</param>
+    /// <typeparam name="T">任意のパラメーター</typeparam>
+    /// <returns>要素を省いて追加してList<T>を返す</returns>
+    public static List<T> AddTriming<T>(this IList<T> eList, T addObj) where T : IComparable<T>
+    {
+        List<T> addListObj = new List<T>();
+        if(addListObj.Contains(addObj) != false)
+        {
+            // 何もしない
+        }
+        else
+        {
+            addListObj.Add(addObj);
+        }
+        return addListObj;
+    }
+}
 
 public class MouseBehaviour : MonoBehaviour
 {
@@ -18,10 +42,11 @@ public class MouseBehaviour : MonoBehaviour
     /// </summary>
     void Start()
     {
-        // マウスクリック時の座標を取得
+        // // マウスクリック時の座標を取得
         // this.UpdateAsObservable()
-        //     .Where(_ => Input.GetMouseButtonDown(0))
-        //     .Subscribe(_ => { GetClickObjectName(); });
+        //     .Subscribe(_ => {
+        //         GetObjectByRayCastHit();
+        //     });
         // マウスホールド時の挙動
         this.FixedUpdateAsObservable()
             .Where(_ => Input.GetMouseButton(0))
@@ -41,28 +66,21 @@ public class MouseBehaviour : MonoBehaviour
         vector3Tuple.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, vector3Tuple.screenPoint.z));
         return vector3Tuple;
     }
-    private void GetClickObjectName()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit = new RaycastHit();
-        if(Physics.Raycast(ray.origin, ray.direction, out hit, 100.0f))
-        {
-            Debug.Log(hit.collider.gameObject.name);
-        }
-    }
-
+    
     private List<GameObject> objectList = new List<GameObject>();
-    private GameObject GetObjectByRayCastHit()
+    private List<GameObject> GetObjectByRayCastHit()
     {
-        GameObject test = (default);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit = new RaycastHit();
         if(Physics.Raycast(ray.origin, ray.direction, out hit, 100.0f))
         {
-            objectList.Add(hit.collider.gameObject);
-            Debug.Log(hit.collider.gameObject.name);
+            if(objectList.Contains(hit.collider.gameObject) == false)
+            {
+                objectList.Add(hit.collider.gameObject);
+                Debug.Log(hit.collider.gameObject.name);
+            }
         }
-        return test;
+        return objectList;
     }
     private void GetObjectOnHoldingMouseButton()
     {
