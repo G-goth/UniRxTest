a1ad9f2599924f386a9388371cd14bb2b492990f
GotohFumiaki <gf1010redgreen@gmail.com>
2019-03-21 22:15:19 +0900

    マウスプッシュ時オブジェクトを取得し、リリースで開放の実装

--- Assets/Scripts/MouseBehaviour.cs
+++ Assets/Scripts/MouseBehaviour.cs
@@ -13,9 +13,8 @@ public static class ListExtensions
     /// <param name="addObj">追加したい要素</param>
     /// <typeparam name="T">任意のパラメーター</typeparam>
     /// <returns>要素を省いて追加してList<T>を返す</returns>
-    public static List<T> AddTriming<T>(this IList<T> extList, T addObj)
+    public static void AddTriming<T>(this IList<T> extList, T addObj)
     {
-        // List<T> addListObj = new List<T>();
         if(extList.Contains(addObj) != false)
         {
             // 何もしない
@@ -24,16 +23,20 @@ public static class ListExtensions
         {
             extList.Add(addObj);
         }
-        return extList.ToList();
     }
 }
 
 public class MouseBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private Material _material;
+    [SerializeField]
+    private Material _defMaterial;
     private Vector3 screenPoint;
     private Vector3 offset;
     private Vector3 currentScreenPoint;
     private Vector3 currentPosition;
+    private List<GameObject> objectList = new List<GameObject>();
     private BoolReactiveProperty isClick = new BoolReactiveProperty();
 
     /// <summary>
@@ -53,6 +56,17 @@ public class MouseBehaviour : MonoBehaviour
             .Subscribe(_ => {
                 GetObjectByRayCastHit();
             });
+
+        // マウスクリックリリース時の挙動
+        this.FixedUpdateAsObservable()
+            .Where(_ => Input.GetMouseButtonUp(0))
+            .Subscribe(_ => {
+                foreach(var obj in objectList)
+                {
+                    obj.GetComponent<Renderer>().material = _defMaterial;
+                }
+                objectList.Clear();
+            });
     }
 
     /// <summary>
@@ -67,7 +81,6 @@ public class MouseBehaviour : MonoBehaviour
         return vector3Tuple;
     }
     
-    private List<GameObject> objectList = new List<GameObject>();
     private List<GameObject> GetObjectByRayCastHit()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -75,7 +88,10 @@ public class MouseBehaviour : MonoBehaviour
         if(Physics.Raycast(ray.origin, ray.direction, out hit, 100.0f))
         {
             objectList.AddTriming(hit.collider.gameObject);
-            // Enumerable.Range(0, objectList.Count).Select((int index, GameObject obj) => { return; });
+            foreach(var obj in objectList)
+            {
+                obj.GetComponent<Renderer>().material = _material;
+            }
         }
         return objectList;
     }
